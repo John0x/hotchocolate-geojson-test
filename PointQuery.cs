d46c8c3b5e6da9aa01573595ad09e93fed62d2ef
Moritz Brandes <mb@grannyandsmith.com>
2020-03-02 16:39:17 +0100

    added point test query

---
+++ PointQuery.cs
@@ -0,0 +1,14 @@
+using NetTopologySuite.Geometries;
+
+namespace StarWars
+{
+    public class PointQuery
+    {
+        public Point Hello()
+        {
+            var p = new Point(1, 1);
+            
+            return p;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using HotChocolate;
+using HotChocolate.AspNetCore;
+using HotChocolate.Subscriptions;
+
+
+namespace StarWars
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // Add in-memory event provider
+            services.AddInMemorySubscriptionProvider();
+
+            // Add GraphQL Services
+            services.AddGraphQL(sp => SchemaBuilder.New()
+                .AddServices(sp)
+                .AddQueryType(d => d.Name("Query"))
+                .AddType<PointQuery>()
+
+                .Create());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+                .UseRouting()
+                .UseWebSockets()
+                .UseGraphQL("/graphql")
+                .UsePlayground("/graphql", "/graphql");
+        }
+    }
+}
